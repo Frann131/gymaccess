d62c86d97f2f32c5f3583019d733fff12a2e70c7
Frann131 <franmindurry@gmail.com>
2024-01-31 18:47:31 -0300

    form

--- client/src/components/CrearUserForm/CrearUserForm.jsx
+++ client/src/components/CrearUserForm/CrearUserForm.jsx
@@ -3,18 +3,50 @@ const CrearUserForm = () => {
     <div>
       <form action="submit">
         <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
+          <label for="nameinput">Nombre y Apellido</label>
+          <input
+            type="text"
+            class="form-control"
+            id="nameinput"
+            placeholder="Nombre y apellido"
+          />
+        </div>
+        <div class="form-group">
+          <label for="emailinput">Correo electrónico</label>
           <input
             type="email"
             class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
+            id="emailinput"
+            placeholder="Correo electrónico"
           />
-          <small id="emailHelp" class="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
         </div>
+        <div class="form-group">
+          <label for="dniinput">DNI/Pasaporte</label>
+          <input
+            type="number"
+            class="form-control"
+            id="dniinput"
+            placeholder="Ingresar número de documento"
+          />
+        </div>
+        <div class="form-group">
+          <label for="phoneinput">Teléfono</label>
+          <input
+            type="number"
+            class="form-control"
+            id="phoneinput"
+            placeholder="Con codigo de área, sin 0 ni 15"
+          />
+        </div>
+        <fieldset>
+        <legend>Tipo de suscripción</legend>
+        <div class="form-group flex-row" >
+          <label for="mensualinput">Mensual</label>
+          <input type="radio" class="form-check-input" value="" id="mensualinput" />
+          <label for="fiftyinput">Quincena</label>
+          <input type="radio" class="form-check-input" value="" id="fiftyinput" />
+        </div>
+        </fieldset>
       </form>
     </div>
   );
